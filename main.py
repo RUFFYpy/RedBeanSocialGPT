200c9dbb083edadcc20a11950d77a9726946b527
RUFFYpy <143926032+RUFFYpy@users.noreply.github.com>
2023-09-05 03:52:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,167 +1,56 @@
-import tkinter as tk
-from tkinter import ttk
-import csv
 import random
-import time
-import json
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from bs4 import BeautifulSoup as bs
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.proxy import Proxy, ProxyType
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import ui
-from config import proxies
-from utils import (
-    generate_sensor_data,
-    solve_captcha,
-    handle_email,
-)
-from actions import login, boost_karma
 
-# Your CapMonster and API integration code would go here
-
-def create_instagram_account(email, username, password, cookies):
-    try:
-        # Your Instagram account creation code using Selenium here
-        # Make sure to use the provided email, username, password, and cookies
-
-        # Example:
-        print(f"Creating Instagram account with email: {email}, username: {username}, password: {password}")
-        # Your Instagram account creation logic here...
-
-        return True  # Indicate success
-    except Exception as error:
-        return False, error  # Return an error message in case of failure
-
-def create_reddit_account(email, username, password, cookies):
-    try:
-        # Your Reddit account creation code using Selenium here
-        # Make sure to use the provided email, username, password, and cookies
-
-        # Example:
-        print(f"Creating Reddit account with email: {email}, username: {username}, password: {password}")
-        # Your Reddit account creation logic here...
-
-        return True  # Indicate success
-    except Exception as error:
-        return False, error  # Return an error message in case of failure
-
-def create_tiktok_account(email, username, password, cookies):
-    try:
-        # Your TikTok account creation code using Selenium here
-        # Make sure to use the provided email, username, password, and cookies
-
-        # Example:
-        print(f"Creating TikTok account with email: {email}, username: {username}, password: {password}")
-        # Your TikTok account creation logic here...
-
-        return True  # Indicate success
-    except Exception as error:
-        return False, error  # Return an error message in case of failure
-
-def generate_accounts(num_accounts, selected_platforms):
-    # Generate random account data (placeholder)
+from create_accounts import Ig_Account_Logic, Reddit_Account_Logic, TikTok_Account_Logic
+# Function to read account data from a text file
+def read_account_data(file_path):
     accounts = []
-    for _ in range(num_accounts):
-        username = f"user{random.randint(1000, 9999)}"
-        password = f"password{random.randint(1000, 9999)}"
-        email = f"{username}@example.com"
-        accounts.append([username, password, email])
+    with open(file_path, 'r') as file:
+        for line in file:
+            username, password, email = line.strip().split(',')
+            accounts.append((username, password, email))
+    return accounts
 
-    # Save account data to a CSV file
-    with open('accounts.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Username', 'Password', 'Email'])
-        writer.writerows(accounts)
+# Function to read proxy data from a text file
+def read_proxy_data(file_path):
+    proxies = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            proxy = line.strip()
+            proxies.append(proxy)
+    return proxies
 
-def main():
-    # Step 1: Create a GUI
-    root = tk.Tk()
-    root.title("Account Generation Script")
+# Specify the path to your accounts.txt and proxy.txt files
+accounts_file_path = 'accounts.txt'
+proxy_file_path = 'proxy.txt'
 
-    num_accounts_label = ttk.Label(root, text="Number of Accounts:")
-    num_accounts_label.pack()
-    num_accounts_entry = ttk.Entry(root)
-    num_accounts_entry.pack()
+# Read account data from the file
+account_data = read_account_data(accounts_file_path)
 
-    generate_button = ttk.Button(root, text="Generate Accounts", command=generate_button_clicked)
-    generate_button.pack()
+# Read proxy data from the file
+proxies = read_proxy_data(proxy_file_path)
 
-    root.mainloop()
+# Initialize an empty list to store account details
+account_cookies = []
 
-def generate_button_clicked():
-    num_accounts = int(num_accounts_entry.get())
-    selected_platforms = ["Instagram", "Reddit", "TikTok"]  # Placeholder for selected platforms
-    generate_accounts(num_accounts, selected_platforms)
+# Loop through the account data and create accounts on each platform
+for username, password, email in account_data:
+    # Randomly select a proxy from the list
+    selected_proxy = random.choice(proxies)
 
-if __name__ == "__main__":
-    main()
-def main():
-    # Step 1: Create a GUI
-    root = tk.Tk()
-    root.title("Account Generation Script")
+    # Create Instagram account
+    instagram_result = create_instagram_account(email, username, password, account_cookies, selected_proxy)
+    if instagram_result:
+        print(f"Instagram account created: {username}")
 
-    num_accounts_label = ttk.Label(root, text="Number of Accounts:")
-    num_accounts_label.pack()
-    num_accounts_entry = ttk.Entry(root)
-    num_accounts_entry.pack()
+    # Create Reddit account
+    reddit_result = create_reddit_account(email, username, password, account_cookies, selected_proxy)
+    if reddit_result:
+        print(f"Reddit account created: {username}")
 
-    generate_button = ttk.Button(root, text="Generate Accounts", command=generate_button_clicked)
-    generate_button.pack()
+    # Create TikTok account
+    tiktok_result = create_tiktok_account(email, username, password, account_cookies, selected_proxy)
+    if tiktok_result:
+        print(f"TikTok account created: {username}")
 
-    root.mainloop()
-
-def generate_button_clicked():
-    num_accounts = int(num_accounts_entry.get())
-    selected_platforms = ["Instagram", "Reddit", "TikTok"]  # Placeholder for selected platforms
-
-    accounts = []
-    cookies = []
-
-    # Generate accounts for selected platforms
-    for platform in selected_platforms:
-        if platform == "Instagram":
-            create_func = create_instagram_account
-        elif platform == "Reddit":
-            create_func = create_reddit_account
-        elif platform == "TikTok":
-            create_func = create_tiktok_account
-
-        for _ in range(num_accounts):
-            result = create_func(f"user{_}", f"password{_}", f"user{_}@example.com", cookies)
-            if result[0]:
-                accounts.append(result[1])
-                cookies.append(result[2])
-            else:
-                print(f"Failed to create {platform} account: {result[1]}")
-
-    # Save account data to a CSV file
-    with open('accounts.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Username', 'Password', 'Email'])
-        writer.writerows(accounts)
-
-    # Perform karma boosting on Reddit
-    boost_count = 5  # Number of karma boosting actions
-    sub_reddit = "your_subreddit_here"  # Replace with the subreddit you want to use
-
-    if len(cookies) > 0:
-        print("\n==================================Commencing Karma Boosting==================================\n")
-
-        while boost_count > 0:
-            for cookie in cookies:
-                result = boost_karma(cookie["cookies"], cookie["headers"], sub_reddit, cookie["user_details"]["access_token"]["token"], cookie["user_details"]["username"], proxies)
-                if result[0]:
-                    print(result[1])
-                else:
-                    print(result[1])
-            boost_count -= 1
-            time.sleep(600)
-
-if __name__ == "__main__":
-    main()
+# At this point, account_cookies will contain the details of the created accounts
+# You can use this data for further actions or storage

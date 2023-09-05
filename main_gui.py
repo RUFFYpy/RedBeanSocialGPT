816f15590f7232be30aa853b27f06004d31879e8
RUFFYpy <143926032+RUFFYpy@users.noreply.github.com>
2023-09-05 04:05:57 -0700

    Create main_gui.py

---
+++ main_gui.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import ttk
+
+def generate_button_clicked():
+    num_accounts = int(num_accounts_entry.get())
+    selected_platforms = ["Instagram", "Reddit", "TikTok"]  # Placeholder for selected platforms

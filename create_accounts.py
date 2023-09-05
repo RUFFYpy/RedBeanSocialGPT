a5d95b3ea34031e97b4d305b5a2fd3715ebb5851
RUFFYpy <143926032+RUFFYpy@users.noreply.github.com>
2023-09-05 04:07:16 -0700

    Create create_accounts.py

---
+++ create_accounts.py
@@ -0,0 +1,16 @@
+# create_accounts.py
+import random
+
+# Function to read account data from a text file
+def read_account_data(file_path):
+    # ...
+
+# Function to read proxy data from a text file
+def read_proxy_data(file_path):
+    # ...
+
+# Specify the path to your accounts.txt and proxy.txt files
+accounts_file_path = 'accounts.txt'
+proxy_file_path = 'proxy.txt'
+
+# Account creation and proxy handling code...
